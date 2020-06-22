9e1c82f8e95503ab5e9584fbe0b3b86ecd5e46bc
keeprich <keeprich@keeprichs-air.lan>
2020-06-22 07:19:02 +0100

    added style

--- index.php
+++ index.php
@@ -24,6 +24,7 @@ crawl_page($url, 2)
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 <body>
